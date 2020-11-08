9b45538497efc45bece52861ed0f8d43fee09625
Trevor Linton <trevor.linton@gmail.com>
2020-11-07 20:32:06 -0700

    added logging

---
+++ internal/debug/log.go
@@ -0,0 +1,38 @@
+package debug
+
+import (
+	"log"
+	"os"
+	"io/ioutil"
+)
+
+var loggerInfo *log.Logger
+var loggerError *log.Logger
+var loggerDebug *log.Logger
+
+func Infof(format string, v ...interface{}) {
+	loggerInfo.Printf(format, v...)
+}
+
+func Debugf(format string, v ...interface{}) {
+	loggerDebug.Printf(format, v...)
+}
+
+func Errorf(format string, v ...interface{}) {
+	loggerError.Printf(format, v...)
+}
+
+func Fatalf(format string, v ...interface{}) {
+	loggerInfo.Fatalf(format, v...)
+}
+
+func init() {
+	loggerInfo = log.New(os.Stdout, "", log.Lshortfile)
+	loggerError = log.New(os.Stderr, "ERROR: ", log.Lshortfile)
+	if os.Getenv("DEBUG") == "true" {
+		loggerDebug = log.New(os.Stdout, "DEBUG: ", log.Lshortfile)
+	} else {
+		loggerDebug = log.New(ioutil.Discard, "", log.Lshortfile)
+	}
+	loggerInfo = log.New(os.Stdout, "", log.Lshortfile)
+}
