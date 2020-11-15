15d264ecb0e10e290af497d5b0904fea00c9d7bb
Trevor Linton <trevor.linton@gmail.com>
2020-11-14 22:08:23 -0700

    Syslog tls codacy fixed

--- pkg/input/syslogtls/syslogtls.go
+++ pkg/input/syslogtls/syslogtls.go
@@ -10,45 +10,46 @@ import (
 	"time"
 )
 
-type HandlerSyslogTls struct {
+/* Handles Syslog TLS inputs */
+type HandlerSyslogTLS struct {
 	errors      chan error
 	packets     chan syslog.Packet
 	stop        chan struct{}
 	channel     server.LogPartsChannel
 	server      *server.Server
-	server_name string
-	key_pem     string
-	cert_pem    string
-	ca_pem      string
+	serverName  string
+	keyPem      string
+	certPem     string
+	caPem       string
 	address     string
 }
 
-func (handler *HandlerSyslogTls) getServerConfig() (*tls.Config, error) {
+func (handler *HandlerSyslogTLS) getServerConfig() (*tls.Config, error) {
 	capool, err := x509.SystemCertPool()
 	if err != nil {
 		capool = x509.NewCertPool()
 	}
-	if handler.ca_pem != "" {
-		if ok := capool.AppendCertsFromPEM([]byte(handler.ca_pem)); !ok {
+	if handler.caPem != "" {
+		if ok := capool.AppendCertsFromPEM([]byte(handler.caPem)); !ok {
 			return nil, errors.New("Unable to parse pem.")
 		}
 	}
 
-	cert, err := tls.X509KeyPair([]byte(handler.cert_pem), []byte(handler.key_pem))
+	cert, err := tls.X509KeyPair([]byte(handler.certPem), []byte(handler.keyPem))
 	if err != nil {
 		return nil, err
 	}
 
 	config := tls.Config{
 		Certificates: []tls.Certificate{cert},
-		ServerName:   handler.server_name,
+		ServerName:   handler.serverName,
 		RootCAs:      capool,
 	}
 	config.Rand = rand.Reader
 	return &config, nil
 }
 
-func (handler *HandlerSyslogTls) Close() error {
+func (handler *HandlerSyslogTLS) Close() error {
 	handler.stop <- struct{}{}
 	handler.server.Kill()
 	close(handler.packets)
@@ -67,7 +68,7 @@ func defaultTlsPeerName(tlsConn *tls.Conn) (tlsPeer string, ok bool) {
 	return cn, true
 }
 
-func (handler *HandlerSyslogTls) Dial() error {
+func (handler *HandlerSyslogTLS) Dial() error {
 	if handler.server != nil {
 		return errors.New("Dial may only be called once.")
 	}
@@ -129,29 +130,29 @@ func (handler *HandlerSyslogTls) Dial() error {
 	return nil
 }
 
-func (handler *HandlerSyslogTls) Errors() chan error {
+func (handler *HandlerSyslogTLS) Errors() chan error {
 	return handler.errors
 }
 
-func (handler *HandlerSyslogTls) Packets() chan syslog.Packet {
+func (handler *HandlerSyslogTLS) Packets() chan syslog.Packet {
 	return handler.packets
 }
 
-func (handler *HandlerSyslogTls) Pools() bool {
+func (handler *HandlerSyslogTLS) Pools() bool {
 	return true
 }
 
-func Create(server_name string, key_pem string, cert_pem string, ca_pem string, address string) (*HandlerSyslogTls, error) {
-	return &HandlerSyslogTls{
+func Create(serverName string, keyPem string, certPem string, caPem string, address string) (*HandlerSyslogTLS, error) {
+	return &HandlerSyslogTLS{
 		errors:      make(chan error, 1),
 		packets:     make(chan syslog.Packet, 100),
 		stop:        make(chan struct{}, 1),
 		channel:     make(server.LogPartsChannel),
 		server:      nil,
-		server_name: server_name,
-		key_pem:     key_pem,
-		cert_pem:    cert_pem,
-		ca_pem:      ca_pem,
+		serverName: serverName,
+		keyPem:     keyPem,
+		certPem:    certPem,
+		caPem:      caPem,
 		address:     address,
 	}, nil
 }
